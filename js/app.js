1bda0dd7d9c6ce65937b8287011b90b2ae026751
Richard <MYCRAFTisbest@gmail.com>
2020-11-10 19:58:53 -0500

    I am the Smart
    SMRT

---
+++ js/app.js
@@ -0,0 +1,151 @@
+$(document).ready(function() {
+    $('.addEnchantBtn').click(addEnchant)
+    $('.type').change(function() {
+        if (
+            $(this)
+                .children('select')
+                .val() != 'Book'
+        ) {
+            $(this)
+                .siblings('.enchant')
+                .html('')
+        }
+    })
+    $('.findCost').click(setFinalCost)
+})
+
+function addEnchant() {
+    let tool_type = $(this)
+        .parent()
+        .siblings('.type')
+        .children('select')
+        .val()
+    let total_enchant = $(this)
+        .parent()
+        .siblings('.enchant')
+        .children().length
+    // console.log(total_enchant)
+    let enchantSelectNode = $(
+        `<div class="enchant_${total_enchant}"><select class="enchant_name">
+        
+        </select>
+        <select class="enchant_lv">
+        
+        </select>
+        <button class="enchant_remove removeEnchantBtn">X</button></div>`
+    )
+
+    // Add enchant select node
+    $(this)
+        .parent()
+        .siblings('.enchant')
+        .append(enchantSelectNode)
+
+    let selectedNames = []
+    for (i = 0; i < total_enchant; i += 1) {
+        selectedNames.push(
+            $(this)
+                .parent()
+                .siblings('.enchant')
+                .children(`.enchant_${i}`)
+                .children('.enchant_name')
+                .val()
+        )
+    }
+
+    // Add applicable enchantment list
+    for (name of applicable_enchant[tool_type]) {
+        if (!selectedNames.includes(name)) {
+            $(this)
+                .parent()
+                .siblings('.enchant')
+                .children(`.enchant_${total_enchant}`)
+                .children('.enchant_name')
+                .append($(`<option>${name}</option>`))
+        }
+    }
+
+    function getLevelOption(name) {
+        var max_lv = enchant_list[name].max
+
+        let optionHtml = ''
+        for (let i = 1; i <= max_lv; i += 1) {
+            optionHtml += `<option>${i}</option>`
+        }
+        return optionHtml
+    }
+
+    // Add applicable enchantment level for new enchantment
+    let enchant_name = $(this)
+        .parent()
+        .siblings('.enchant')
+        .children(`.enchant_${total_enchant}`)
+        .children('.enchant_name')
+        .val()
+    $(this)
+        .parent()
+        .siblings('.enchant')
+        .children(`.enchant_${total_enchant}`)
+        .children('.enchant_lv')
+        .html(getLevelOption(enchant_name))
+
+    // Create event that update applicable enchantment level everytime enchantment name is changed
+    $(this)
+        .parent()
+        .siblings('.enchant')
+        .children(`.enchant_${total_enchant}`)
+        .children('.enchant_name')
+        .change(function() {
+            let name = $(this).val()
+            $(this)
+                .siblings('.enchant_lv')
+                .html(getLevelOption(name))
+        })
+
+    //Create event for remove button
+    $('.removeEnchantBtn').click(removeEnchant)
+}
+
+function removeEnchant() {
+    const enchant = $(this).parent();
+    const enchantments = enchant.parent();
+    enchant.remove();
+    //re-create enchantments index
+    enchantments.children().each((i, elem) => {
+        $(elem).attr('class', `enchant_${i}`);
+    });
+}
+
+function getTool(which) {
+    tool = {
+        type: $(`#${which} > .type > select`).val(),
+        enchantments: {},
+        prior_penalty: Number($(`#${which}_penalty`).val())
+    }
+
+    for (i = 0; i < $(`#${which} > .enchant`).children().length; i += 1) {
+        let enchant_name = $(
+            `#${which} > .enchant > .enchant_${i} > .enchant_name`
+        ).val()
+        if (Object.keys(enchant_list).includes(enchant_name)) {
+            let enchant_lv = $(
+                `#${which} > .enchant > .enchant_${i} > .enchant_lv`
+            ).val()
+            tool['enchantments'][enchant_name] = Number(enchant_lv)
+        }
+    }
+    return tool
+}
+
+function setFinalCost() {
+    const [total_cost, result_tool] = findCost()
+    console.log(total_cost, result_tool)
+    $('#total_cost').text('Cost : ' + total_cost)
+
+    let tool_summary_text = ''
+    for (enchant in result_tool.enchantments) {
+        tool_summary_text +=
+            enchant + ' ' + result_tool.enchantments[enchant] + '<br/>'
+    }
+    $('#result_tool').html(tool_summary_text)
+}
---
+++ js/logic.js
@@ -0,0 +1,152 @@
+function findCost() {
+    function isConflictWithTarget(name) {
+        var isConflict = false
+        for (conflict_group of conflict) {
+            if (conflict_group.includes(name)) {
+                for (other_name of conflict_group) {
+                    if (
+                        other_name != name &&
+                        Object.keys(target.enchantments).includes(other_name)
+                    ) {
+                        isConflict = true
+                        $('#log').append(
+                            name + ' is incompatible with ' + other_name + '!'
+                        )
+                    }
+                }
+            }
+        }
+        return isConflict
+    }
+
+    $('#log').text('') // reset textarea
+
+    target = getTool('target')
+    sacrifice = getTool('sacrifice')
+
+    if ($('#swapTool').is(':checked')) {
+        target = getTool('sacrifice')
+        sacrifice = getTool('target')
+    }
+
+    total_cost = 0
+
+    // For each enchantment on the sacrifice:
+    for (name in sacrifice.enchantments) {
+        // Ignore any enchantment that cannot be applied to the target (e.g. Protection on a sword).
+        if (!applicable_enchant[target.type].includes(name)) {
+            // console.log(name + ' not applicable')
+            $('#log').append(name + ' not applicable')
+            $('#log').append('\n')
+            continue
+        }
+
+        // Add one level for every incompatible enchantment on the target (In Java Edition).
+        if (isConflictWithTarget(name)) {
+            // console.log(name + ' has conflict. Cost: 1')
+            $('#log').append(' Cost: 1')
+            $('#log').append('\n')
+            total_cost += 1
+        }
+
+        //If the enchantment is compatible
+        else {
+            // If the target has the enchantment as well
+            if (name in target.enchantments) {
+                // console.log(
+                //     'Combining ' +
+                //         name +
+                //         ' ' +
+                //         target.enchantments[name] +
+                //         ' and ' +
+                //         name +
+                //         ' ' +
+                //         sacrifice.enchantments[name] +
+                //         '...'
+                // )
+                $('#log').append(
+                    'Combining ' +
+                        name +
+                        ' ' +
+                        target.enchantments[name] +
+                        ' and ' +
+                        name +
+                        ' ' +
+                        sacrifice.enchantments[name] +
+                        '...'
+                )
+                $('#log').append('\n')
+                //If sacrifice level is equal, the target gains one level, unless it is already at the maximum level for that enchantment.
+                if (target.enchantments[name] == sacrifice.enchantments[name]) {
+                    target.enchantments[name] = Math.min(
+                        target.enchantments[name] + 1,
+                        enchant_list[name].max
+                    )
+                }
+                //If sacrifice level is greater, the target is raised to the sacrifice's level
+                else if (
+                    target.enchantments[name] < sacrifice.enchantments[name]
+                ) {
+                    target.enchantments[name] = sacrifice.enchantments[name]
+                }
+            }
+
+            //If the target does not have the enchantment, it gains all levels of that enchantment
+            else {
+                target.enchantments[name] = sacrifice.enchantments[name]
+            }
+            // $('#log').append(
+            //     "Target's " + name + ' becomes ' + target.enchantments[name]
+            // )
+
+            // For Java Edition, add the final level of the enchantment on the resulting item multiplied by the multiplier from the table below.
+            multiplier = enchant_list[name].mul_item
+            if (sacrifice.type == 'Book') {
+                multiplier = enchant_list[name].mul_book
+            }
+            cost = multiplier * target.enchantments[name]
+            total_cost += cost
+            $('#log').append(
+                name +
+                    ' ' +
+                    target.enchantments[name] +
+                    '. Multiplier: ' +
+                    multiplier +
+                    '. Cost: ' +
+                    cost
+            )
+            $('#log').append('\n')
+        }
+
+        // $('#log').append('Total cost: ' + total_cost)
+        // $('#log').append('\n')
+    }
+
+    //Calculate Penalty cost
+    let target_penalty = target.prior_penalty
+    let sacrifice_penalty = sacrifice.prior_penalty
+    let new_penalty = Math.max(target_penalty, sacrifice_penalty)
+    target.prior_penalty = new_penalty + 1
+
+    let target_penalty_cost = Math.pow(2, target_penalty) - 1
+    let sacrifice_penalty_cost = Math.pow(2, sacrifice_penalty) - 1
+    let total_penalty_cost = target_penalty_cost + sacrifice_penalty_cost
+    total_cost += total_penalty_cost
+    $('#log').append(
+        'Penalty Cost: ' +
+            target_penalty_cost +
+            ' + ' +
+            sacrifice_penalty_cost +
+            ' = ' +
+            total_penalty_cost
+    )
+    $('#log').append('\n')
+
+    $('#log').append('Final Cost: ')
+    $('#log').append(total_cost)
+    $('#log').append('\n')
+
+    // console.log(target)
+
+    return [total_cost, target]
+}
